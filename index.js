301f930cfd1619862026e0e7c8d7b6ac27793e7e
Anton Petrov <52886942+webdirection-dev@users.noreply.github.com>
2022-03-10 03:12:00 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
-    
+
     const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
     const token = "0942518e753e88745cd945cf0bd40269b0b9a937";
 
@@ -121,16 +121,18 @@ window.addEventListener('DOMContentLoaded', () => {
         // general
         arrForRender.forEach((item, index) => {
             const {value, data} = item
+            let classTitle = ""
+            if (item.data.state.status !== 'ACTIVE') classTitle = 'variant__title'
 
             if (index < 5) {
                 const elem = document.createElement('div')
                 elem.classList.add('variant__item')
 
                 elem.innerHTML = `
-                        <div class="variant__title">${markLetters(value, valueInput)}</div>
+                        <div class=${classTitle}>${markLetters(value, valueInput)}</div>
                         <div class="variant__description">
-                            <p class="variant__inn">${data.inn}</p>
-                            <p class="variant__own">${data.address === null ? '' : data.address.value}</p>
+                            <p class="variant__inn">${markLetters(data.inn, valueInput)}</p>
+                            <p class="variant__own">${data.address === null ? '' : markLetters(data.address.value, valueInput)}</p>
                         </div>
                     `
                 elem.addEventListener('click', (event) => {
@@ -171,17 +173,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //покрасить искомые символы в найденных словах
     function markLetters(str, valueInput) {
-        const isIndexOf = str.toLowerCase().indexOf(valueInput)
+        const isIndexOf = str.toLowerCase().indexOf(valueInput.toLowerCase())
 
-        return str.slice(0, isIndexOf) +
-            '<span class="variant__mark">' +
-            str.slice(isIndexOf, isIndexOf+valueInput.length) +
-            '</span>' +
-            str.slice(isIndexOf+valueInput.length)
+        if (isIndexOf >= 0) {
+            return str.slice(0, isIndexOf) +
+                '<span class="variant__mark">' +
+                str.slice(isIndexOf, isIndexOf+valueInput.length) +
+                '</span>' +
+                str.slice(isIndexOf+valueInput.length)
+        } else return str
     }
 
     // fetch
+    async function getIP() {
+        let urlIp = 'http://api.ipstack.com/check?access_key=93744030badf23ec17f6c5613a7da447'
+        try {
+            const response = await fetch( urlIp, {});
+
+            if (!response.ok) {
+                throw new Error('ServerError!')
+            }
+
+            const data = await response.json();
+
+            localStorage.setItem('city', data.city)
+            return true
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     async function getData(query) {
+        if (localStorage.getItem('city') === null) await getIP()
+        let city = localStorage.getItem('city')
         try {
             const response = await fetch( url, {
                 method: "POST",
@@ -201,7 +225,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const data = await response.json();
 
-            daData = data.suggestions
+            const withCity = data.suggestions.filter(item => item.data.address.data.city === 'Москва')
+            const withoutCity = data.suggestions.filter(item => item.data.address.data.city !== 'Москва')
+            const filterOnCity = [...withCity, ...withoutCity]
+
+            if (city !== null) daData = filterOnCity
+            else daData = data.suggestions
+            console.log(data.suggestions)
         } catch (error) {
             console.log(error.message)
         }
